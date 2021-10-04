3f34cb2c103a5c577381b0c6335e787bc063873b
Ralf Prezia <62730168+ralfprezia@users.noreply.github.com>
2021-10-04 18:46:34 -0300

    Add files via upload

---
+++ cypress/support/commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//criação dos comandos customizados para chamar a função no ticket.spec.js
+
+
+//const cypress = require("cypress");
+
+Cypress.Commands.add("fillMandatoryFields", data => {
+    cy.get("#first-name").type(data.firstName);
+    cy.get("#last-name").type(data.lastName);
+    cy.get("#email").type(data.email);
+    cy.get("#agree").check();
+})
+
+
